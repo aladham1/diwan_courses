11b8c50b4112230693a43b698483b1b8750dae88
aladham1 <63999566+aladham1@users.noreply.github.com>
2023-07-23 12:33:00 +0300

    Update RedirectIfAuthenticated.php

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -20,7 +20,7 @@ class RedirectIfAuthenticated
      */
     public function handle(Request $request, Closure $next, ...$guards)
     {
-        if ($this->auth->guest())
+        if (auth()->guest())
         {
             if ($request->ajax())
             {
