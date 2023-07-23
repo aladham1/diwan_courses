c9ddf420e434c921cad8a2ec233ef244a60acb82
aladham1 <63999566+aladham1@users.noreply.github.com>
2023-07-23 14:13:56 +0300

    Update Authenticate.php

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -3,17 +3,19 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
-use Slides\Saml2\Models\Tenant;
 
 class Authenticate extends Middleware
 {
-   
+    /**
+     * Get the path the user should be redirected to when they are not authenticated.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return string|null
+     */
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            $tenant = Tenant::where('key', 'ssoidp')->first();
-            $redirectTo = saml_url($request->fullUrl(), $tenant->uuid);
-            return  redirect($redirectTo);
+            return route('login');
         }
     }
 }
