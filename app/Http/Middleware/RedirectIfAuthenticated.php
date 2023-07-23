1000dd0c31d6986affb824d298c69614564ab9dc
aladham1 <63999566+aladham1@users.noreply.github.com>
2023-07-23 12:32:16 +0300

    a

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -6,6 +6,7 @@ use App\Providers\RouteServiceProvider;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Slides\Saml2\Models\Tenant;
 
 class RedirectIfAuthenticated
 {
@@ -19,11 +20,18 @@ class RedirectIfAuthenticated
      */
     public function handle(Request $request, Closure $next, ...$guards)
     {
-        $guards = empty($guards) ? [null] : $guards;
+        if ($this->auth->guest())
+        {
+            if ($request->ajax())
+            {
+                return response('Unauthorized.', 401); // Or, return a response that causes client side js to redirect to '/routesPrefix/myIdp1/login'
+            }
+            else
+            {
+                $tenant = Tenant::where('key', 'ssoidp')->first();
+                $redirectTo = saml_url($request->fullUrl(), $tenant->uuid);
 
-        foreach ($guards as $guard) {
-            if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
+                return  redirect($redirectTo);
             }
         }
 
