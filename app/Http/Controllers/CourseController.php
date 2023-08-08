18e4716063c680b5934827e3cbbef95cbef830f1
aladham1 <63999566+aladham1@users.noreply.github.com>
2023-08-08 12:04:03 +0300

    Update CourseController.php

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -25,7 +25,7 @@ class CourseController extends Controller
     public function __construct()
     {
         $this->authorizeResource(Course::class,null, [
-            'except' => [ 'index', 'show' ],
+            'except' => [ 'index' ],
         ]);
     }
 
