43e1ee1624e686fd4ce44cfdaa33bae2ee40ac69
aladham1 <63999566+aladham1@users.noreply.github.com>
2023-08-08 14:03:13 +0300

    a

--- resources/js/core/Setting.js
+++ resources/js/core/Setting.js
@@ -1,6 +1,6 @@
 //window.url_site = 'http://127.0.0.1:8000/';
 //window.website_url = 'http://127.0.0.1:8000/';
 
-https://facbg.gov.ps/diwan_courses/;
+window.url_site = 'https://facbg.gov.ps/diwan_courses/';
 window.website_url = 'https://facbg.gov.ps/diwan_courses/';
 
